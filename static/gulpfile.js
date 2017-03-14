83a7980f09ee06141001ec3e3e311fcb38f05709
hiratasaori <fkyhsa.sh@gmail.com>
2017-03-14 16:32:02 +0900

    [modify] add gulp

---
+++ static/gulpfile.js
@@ -0,0 +1,35 @@
+/* jshint node: true */
+'use strict';
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var plumber = require('gulp-plumber');
+var stylus = require('gulp-stylus');
+var please = require('gulp-pleeease');
+var watchList = {
+    stylus: [
+        './src/stylus/*.styl'
+    ]
+};
+
+var AUTOPREFIXER_BROWSERS = [
+    'last 2 versions',
+    'Android 2.3'
+];
+
+gulp.task('default', function () {
+    gulp.watch(watchList.stylus, ['stylus']);
+});
+
+gulp.task('stylus', function () {
+    return gulp.src('./src/stylus/*.styl')
+        .pipe(plumber())
+        .pipe(stylus())
+        .pipe(please({
+            "autoprefixer": { "browsers": AUTOPREFIXER_BROWSERS },
+            minifier: false,
+            stylus: true
+        }))
+        .pipe(gulp.dest('./css/'))
+        .on('end', reload);
+});
